1a1b5d2979d1f0833724197c980df56d6ebea96b
Ruben Garcia Rojo <rubengarciarojo@Mac-mini-de-Ruben.local>
2026-01-14 21:35:07 +0100

    Refactor: Consolidate Architecture (Split Header/Footer/MainContent) and Fix Imports

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../styles/global.css';
-import logo from '../assets/aspy_logo.png';
+import '../../styles/global.css';
+import logo from '../../assets/aspy_logo.png';
 
 const Header = () => {
     return (
